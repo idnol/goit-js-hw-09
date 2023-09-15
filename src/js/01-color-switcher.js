5c882497561bb9009085e299eba56ba7b612e3bc
Dmytro Shkatula <idnol84@gmail.com>
2023-09-15 23:50:44 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]')
+};
 
+refs.start.addEventListener('click', onChange);
+
+function onChange() {
+  this.setAttribute('disabled', true);
+  document.querySelector('body').style.background = getRandomHexColor();
+  const timerId = setInterval(function() {
+    document.querySelector('body').style.background = getRandomHexColor();
+  }, 1000);
+  refs.stop.addEventListener('click', function() {
+    refs.start.removeAttribute('disabled');
+    clearInterval(timerId);
+  });
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
